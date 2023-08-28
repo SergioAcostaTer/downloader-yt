6390928bd84a4b885f0c163802b26ef53407bdb4
Sergio Acosta <sergioacostaquintana@gmail.com>
2023-08-29 00:11:42 +0100

    first stable version

--- src/services/info.ts
+++ src/services/info.ts
@@ -3,7 +3,7 @@ import { Item } from "../types";
 
 export default async function videoInfo(id: string): Promise<Item> {
   const response: AxiosResponse<Item> = await axios.get(
-    `http://192.168.56.1:3000/video/${id}`
+    `https://downloader-back.onrender.com/video/${id}`
   );
   return response.data;
 }
